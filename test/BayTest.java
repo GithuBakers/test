4604d5688e0f209c24d8432a7848ea5aa6451e7d
XXZ <161250170@mail.smail.nju.edu.cn>
2017-04-11 14:04:46 +0800

    test

---
+++ test/BayTest.java
@@ -0,0 +1,119 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by xuxiangzhe on 2017/4/11.
+ */
+public class BayTest {
+    @Test
+    public void integerRepresentation() throws Exception {
+
+    }
+
+    @Test
+    public void floatRepresentation() throws Exception {
+
+    }
+
+    @Test
+    public void ieee754() throws Exception {
+
+    }
+
+    @Test
+    public void integerTrueValue() throws Exception {
+
+    }
+
+    @Test
+    public void floatTrueValue() throws Exception {
+
+    }
+
+    @Test
+    public void negation() throws Exception {
+
+    }
+
+    @Test
+    public void leftShift() throws Exception {
+
+    }
+
+    @Test
+    public void logRightShift() throws Exception {
+
+    }
+
+    @Test
+    public void ariRightShift() throws Exception {
+
+    }
+
+    @Test
+    public void fullAdder() throws Exception {
+
+    }
+
+    @Test
+    public void claAdder() throws Exception {
+
+    }
+
+    @Test
+    public void oneAdder() throws Exception {
+
+    }
+
+    @Test
+    public void adder() throws Exception {
+
+    }
+
+    @Test
+    public void integerAddition() throws Exception {
+
+    }
+
+    @Test
+    public void integerSubtraction() throws Exception {
+
+    }
+
+    @Test
+    public void integerMultiplication() throws Exception {
+
+    }
+
+    @Test
+    public void integerDivision() throws Exception {
+
+    }
+
+    @Test
+    public void signedAddition() throws Exception {
+
+    }
+
+    @Test
+    public void floatAddition() throws Exception {
+
+    }
+
+    @Test
+    public void floatSubtraction() throws Exception {
+
+    }
+
+    @Test
+    public void floatMultiplication() throws Exception {
+
+    }
+
+    @Test
+    public void floatDivision() throws Exception {
+
+    }
+
+}
