b2c02fcc7318c8da02edb830d23806904376f814
XXZ <161250170@mail.smail.nju.edu.cn>
2017-04-27 17:48:23 +0800

    test2 basic

--- test/XXZTest.java
+++ test/XXZTest.java
@@ -19,7 +19,15 @@ public class XXZTest {
 
     @org.junit.Test
     public void floatRepresentation() throws Exception {
-
+        assertEquals("1100000111100000000000000000000000000000000000000000000000000000",alu.floatRepresentation("-2147483648.0",11,52));
+        assertEquals("01000001001000000000000000000000",alu.floatRepresentation("10.0",8,23));
+        assertEquals("0101000111100",alu.floatRepresentation("47.0",5,7));
+        assertEquals("011111000000",alu.floatRepresentation("2147483647.0",5,6));
+        assertEquals("111111000000",alu.floatRepresentation("-2147483647.0",5,6));
+        assertEquals("10111101110011001100110011001101",alu.floatRepresentation("-0.1",8,23));
+        assertEquals("00111101110011001100110011001101",alu.floatRepresentation("0.1",8,23));
+        assertEquals("000000000000",alu.floatRepresentation("0.0",5,6));
+        assertEquals("01001111100000000000000000000000",alu.floatRepresentation("4294967295.0",8,23));
     }
 
     @org.junit.Test
